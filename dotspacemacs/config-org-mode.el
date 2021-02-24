5d6a351a3d46fafe0d7c937520b6700d3bd2960c
ch-yyk <yyang@dm.ai>
2021-02-23 19:53:49 -0800

    2021-02-23: settings for workday-2 from P52

--- dotspacemacs/config-org-mode.el
+++ dotspacemacs/config-org-mode.el
@@ -3,6 +3,56 @@
   (user/config-org-basics)
   (user/config-org-agenda)
   (user/config-org-captures-templates)
+  (user/config-org-font-size)
+  )
+
+(defun user/config-org-font-size ()
+  ;; header faces
+  (let* ((variable-tuple
+          (cond ((x-list-fonts "Monospaced")         '(:font "Monospaced"))
+                ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+                ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+                ((x-list-fonts "Verdana")         '(:font "Verdana"))
+                ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+                (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+         (base-font-color     (face-foreground 'default nil 'default))
+         (headline           `(:inherit default :weight bold :foreground ,base-font-color))
+         )
+
+    (custom-set-faces
+     `(org-level-8 ((t (,@headline ,@variable-tuple ))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple ))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple ))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple ))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.0))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.2))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.3
+                                   ))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
+  (custom-set-faces
+   '(variable-pitch ((t (:family "Monospace" :height 110 :weight thin))))
+   '(fixed-pitch ((t ( :family "Monospace" :height 130)))))
+
+  (custom-set-faces
+   '(org-block ((t (:inherit fixed-pitch))))
+   '(org-code ((t (:inherit (shadow fixed-pitch)))))
+   '(org-document-info ((t (:foreground "dark orange"))))
+   '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+   '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+   '(org-link ((t (:foreground "royal blue" :underline t))))
+   '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-property-value ((t (:inherit fixed-pitch))) t)
+   '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+   '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+   '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
+
+  ;; buffer font size
+  (add-hook 'org-mode-hook
+            (lambda () (text-scale-decrease 0)))
+  (add-hook 'org-mode-hook 'variable-pitch-mode)
   )
 
 (defun user/config-org-basics()
@@ -19,15 +69,6 @@
 
   (setq org-directory "~/self_project/org-agenda-files/")
 	(setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org") "something"
-
-  (custom-set-faces   ;; set header face in org-mode
-	 '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
-	 '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
-	 '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
-	 '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-	 '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
-	 '(org-property-value ((t (:inherit fixed-pitch :height 0.8))))
-	 )
 
   ;; pomodoro
   (setq org-pomodoro-length 30
